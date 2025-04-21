a6859058453c7014da99e27363d33e7db5dae9d8
ali <nabiy5511@gmail.com>
2025-04-21 14:41:03 +0500

    identificate function fixed for testing.

--- identificate/index.js
+++ identificate/index.js
@@ -4,30 +4,20 @@ const token = urlParams.get('token')
 if (!token) {
     return window.location.href = 'https://aif.uz'
 } else {
-    checkToken()
-}
-
-async function checkToken() {
-    try {
-        const response = await fetch(`https://api.aif.uz/check-token`, {
-            method: 'get',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        const res = await response.json()
-        console.log(res)
-        if (response.ok) {
+    fetch('https://api.aif.uz/check-token', {
+        method: 'get',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
             if (res.status == 'success') {
                 localStorage.setItem('token', token)
                 return window.location.href = 'https://cp.aif.uz/index.html'
             } else {
                 alert(res.message)
             }
-        } else {
-            console.error('Internal server error!')
-        }
-    } catch (error) {
-        console.error('Internal server error!')
-    }
+        })
+        .catch(err => console.error('Internal server error!'))
 }
